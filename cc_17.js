ddf810665416ff64a3c48042023b06fdb83084b8
Pablo Chavez <plchavez@usf.edu>
2025-03-30 20:13:57 -0400

    Task 2 - Created a SalesRep class

--- cc_17.js
+++ cc_17.js
@@ -1,4 +1,4 @@
-//Task 1: Crate a Customer Class
+//Task 1: Create a Customer Class
 class Customer {
     constructor(name, email, purchaseHistory){
         this.name = name;
@@ -13,8 +13,33 @@ class Customer {
     }
 };
 
-const customerA = new Customer("Sam Goodman", "Goodman@gmail.com");
-console.log(`New Customer: ${customer.name}, ${customer.email}`);
-customerA.addPurchase(200);
-customerA.addPurchase(480);
-console.log(`Total amount cusomter ${cusotmer.name} spent: $${customerA.getTotalSpent()}`);
+const sam = new Customer("Sam Goodman", "Goodman@gmail.com");
+sam.addPurchase(200);
+sam.addPurchase(480);
+console.log(`New Customer: ${sam.name}, ${sam.email}`);
+console.log(`Total amount customer ${sam.name} spent: $${sam.getTotalSpent()}`);// Logging new customer creation and total spent after purchases
+
+//Task 2: Create a SalesRep Class
+class SalesRep {
+    constructor(name, clients){
+        this.name = name;
+        this.clients = [];
+    }
+    addClient(customer){
+        this.clients.push(customer)//adding a customer to the list
+    } 
+    getClientTotal(name){
+        const client = this.clients.find(client => client.name ===name);
+        
+        return client ? client.getTotalSpent() : 0;// finding client by name and returning the total they spent
+    }
+};
+
+const jimmy = new Customer("Jimmy", "JimbobSophine@gmail.com");
+jimmy.addPurchase(430);
+const salesRepA = new SalesRep ("Sal");
+salesRepA.addClient(sam);
+salesRepA.addClient(jimmy);
+console.log(`${salesRepA.name}'s clients: ${rep.clients.map(client => client.name).join(",")}`)// shows the sales reps clients
+console.log(`Total spent by Jimmy: $${salesRepA.getClientTotal("Jimmy")}`);
+console.log(`Total spent by Sam: $${salesRepA.getClientTotal("Sam")}`);//shows how much each client spent
